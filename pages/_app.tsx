8ae96de875dc99503fc4e368008791c645e0cb32
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2021-10-29 15:04:17 +0530

    [Fixed] Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,7 +55,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+        {/* <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}> */}
+        {/* FIXME: Temp fix for default light theme */}
+        <ThemeProvider theme={"light"}>
           <div
             style={{
               width: "100vw",
