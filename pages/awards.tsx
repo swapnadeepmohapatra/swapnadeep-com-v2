9018c3154a4503a4ef543f050666a54fe7364fa9
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-05-22 19:04:18 +0530

    [Testing] Award change

--- pages/awards.tsx
+++ pages/awards.tsx
@@ -1,54 +1,66 @@
-import { useEffect, useState } from "react";
-import Head from "next/head";
-import { URL, Bar } from "../utils";
-import { AWARD } from "../interfaces";
-import { Heading1 } from "../components/Heading";
-import { Grid, GridItem, Link, Description, Name } from "../components/Grid";
-import { Main } from "../components/Main";
+// import { useEffect, useState } from "react";
+// import Head from "next/head";
+// import { URL, Bar } from "../utils";
+// import { AWARD } from "../interfaces";
+// import { Heading1 } from "../components/Heading";
+// import { Grid, GridItem, Link, Description, Name } from "../components/Grid";
+// import { Main } from "../components/Main";
 
-export async function getStaticProps() {
-  const res = await fetch(`${URL}/api/all-prizes`);
-  const data = await res.json();
+// export async function getStaticProps() {
+//   const res = await fetch(`${URL}/api/all-prizes`);
+//   const data = await res.json();
 
-  return {
-    props: {
-      staticData: data.prizes,
-    },
-  };
-}
+//   return {
+//     props: {
+//       staticData: data.prizes,
+//     },
+//   };
+// }
 
-function Awards({ staticData }: { staticData: AWARD[] }) {
-  const [data, setData] = useState<AWARD[]>(staticData);
+// function Awards({ staticData }: { staticData: AWARD[] }) {
+//   const [data, setData] = useState<AWARD[]>(staticData);
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(`${URL}/api/all-prizes`);
-      const data = await res.json();
-      setData(data?.prizes);
-    }
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     async function fetchData() {
+//       const res = await fetch(`${URL}/api/all-prizes`);
+//       const data = await res.json();
+//       setData(data?.prizes);
+//     }
+//     fetchData();
+//   }, []);
 
+//   return (
+//     <Main>
+//       <Head>
+//         <title>Awards - Swapnadeep</title>
+//         <meta name="description" content="Awards won by Swapnadeep Mohapatra" />
+//       </Head>
+//       <Heading1>All My Awards</Heading1>
+//       <Grid>
+//         {data?.map((award: AWARD) => (
+//           <Link key={award._id} href={`awards/${award._id}`}>
+//             <GridItem>
+//               <Name>{award.name}</Name>
+//               <Description>{award.honorIssuer}</Description>
+//             </GridItem>
+//           </Link>
+//         ))}
+//       </Grid>
+//       <Bar />
+//     </Main>
+//   );
+// }
+
+// export default Awards;
+
+import type { NextPage } from "next";
+
+const Awards: NextPage = () => {
   return (
-    <Main>
-      <Head>
-        <title>Awards - Swapnadeep</title>
-        <meta name="description" content="Awards won by Swapnadeep Mohapatra" />
-      </Head>
-      <Heading1>All My Awards</Heading1>
-      <Grid>
-        {data?.map((award: AWARD) => (
-          <Link key={award._id} href={`awards/${award._id}`}>
-            <GridItem>
-              <Name>{award.name}</Name>
-              <Description>{award.honorIssuer}</Description>
-            </GridItem>
-          </Link>
-        ))}
-      </Grid>
-      <Bar />
-    </Main>
+    <div>
+      <h1>Awards</h1>
+    </div>
   );
-}
+};
 
 export default Awards;
